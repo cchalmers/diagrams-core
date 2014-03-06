10315e228e6805fcac247bb03422563e7a21dd55
Brent Yorgey <byorgey@gmail.com>
2014-03-06 10:16:22 -0500

    D.C.Types: add Haddock documentation explaining new renderDiaT function and related things

--- src/Diagrams/Core/Types.hs
+++ src/Diagrams/Core/Types.hs
@@ -883,28 +883,34 @@ class (HasLinearMap v, Monoid (Render b v)) => Backend b v where
                  -> Render b v  -- ^ Rendering operation to perform
                  -> Result b v  -- ^ Output of the rendering operation
 
-  -- | XXX
-  --   'adjustDia' allows the backend to make adjustments to the final
+  -- | 'adjustDia' allows the backend to make adjustments to the final
   --   diagram (e.g. to adjust the size based on the options) before
   --   rendering it.  It can also make adjustments to the options
   --   record, usually to fill in incompletely specified size
-  --   information.  A default implementation is provided which makes
+  --   information.  It returns a modified options record, the
+  --   /inverse/ of any transformation applied to the diagram (which
+  --   can be used to transform /e.g./ screen coordinates back into
+  --   local diagram coordinates), and the adjusted diagram itself.
+  --
+  --   A default implementation is provided which makes
   --   no adjustments.  See the diagrams-lib package for other useful
   --   implementations.
   adjustDia :: Monoid' m => b -> Options b v
             -> QDiagram b v m -> (Options b v, Transformation v, QDiagram b v m)
   adjustDia _ o d = (o,mempty,d)
 
-  -- XXX
+  -- | Render a diagram in a way specific to this backend.
   renderDia :: (InnerSpace v, OrderedField (Scalar v), Monoid' m)
             => b -> Options b v -> QDiagram b v m -> Result b v
-  renderDia b opts d = snd (renderDiaWithCoordT b opts d)
+  renderDia b opts d = snd (renderDiaT b opts d)
 
-  -- XXX
-  renderDiaWithCoordT
+  -- | Render a diagram, returning also a transformation which can be
+  --   used to translate output/device coordinates back into local
+  --   diagram coordinates (see 'adjustDia').
+  renderDiaT
     :: (InnerSpace v, OrderedField (Scalar v), Monoid' m)
     => b -> Options b v -> QDiagram b v m -> (Transformation v, Result b v)
-  renderDiaWithCoordT b opts d = (t, doRender b opts' . renderData b $ d')
+  renderDiaT b opts d = (t, doRender b opts' . renderData b $ d')
     where (opts', t, d') = adjustDia b opts d
 
   -- | Backends may override 'renderData' to gain more control over
